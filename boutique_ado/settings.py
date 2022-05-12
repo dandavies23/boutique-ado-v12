f3be2b9be37a6590d97a1d38777e981474ca8cd8
dandavies23 <dan@dandavies23.com>
2022-05-12 20:33:15 +0000

    Fixed Path impoort issue

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -14,14 +14,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# issues with Path BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Build paths inside the project like this: BASE_DIR / 'subdir' issues with Path
-# suggested solution by Sean Young CI
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
